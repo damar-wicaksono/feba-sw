bbb26e0cfc80118169c69a64973663c21dc72152
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-06-11 16:18:48 +0200

    Initial commit with project files

---
+++ febaSW/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("febaSW")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Paul Scherrer Institut")> 
+<Assembly: AssemblyProduct("febaSW")> 
+<Assembly: AssemblyCopyright("Copyright © Paul Scherrer Institut 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5e0dff6a-1a11-4493-b44c-1f941faed2ea")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
