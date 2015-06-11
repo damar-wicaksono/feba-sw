ecd46b4fbe4c2bc4e818c304d953e2a909c97c6c
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-06-11 17:01:41 +0200

    Add rightPad() function

--- febaSW/FortranWriteM.vb
+++ febaSW/FortranWriteM.vb
@@ -1,4 +1,13 @@
-﻿Module FortranWriteM
+﻿Option Explicit On
+''' <summary>
+''' VB.NET functionalities to mimick FORTRAN write formatting
+''' </summary>
+''' <remarks>Converted from VBA to VB.NET</remarks>
+''' <author>
+''' CI41, LRS/PSI, 2014
+''' WD41, LRS/EPFL/PSI, 2015
+''' </author>
+Module FortranWriteM
 
     ''' <summary>
     ''' Pad a string with spaces from the left
@@ -12,10 +21,11 @@
     ''' CI41, LRS/PSI, 2014
     ''' WD41, LRS/EPFL/PSI, 2015
     ''' </author>
-    Public Function leftPad(ByVal strBuffer As String,
-                     ByVal l As Integer) As String
+    Public Function leftPad(ByVal strBuffer As String, _
+                            ByVal l As Integer) As String
 
         Dim intCount As Integer
+
         intCount = l - strBuffer.Length
         If intCount > 0 Then
             leftPad = String.Format("{0}{1}", Space(intCount), strBuffer)
@@ -26,5 +36,30 @@
 
     End Function
 
+    ''' <summary>
+    ''' Pad a string with spaces from the right
+    ''' Think of it as fixed-column, left justified
+    ''' </summary>
+    ''' <param name="strBuffer">The input string</param>
+    ''' <param name="l">the number of columns</param>
+    ''' <returns>String with extra spaces</returns>
+    ''' <remarks>Converted from VBA to VB.NET</remarks>
+    ''' <author>
+    ''' CI41, LRS/PSI, 2014
+    ''' WD41, LRS/EPFL/PSI, 2015
+    ''' </author>
+    Public Function rightPad(ByVal strBuffer As String, _
+                             ByVal l As Integer) As String
 
+        Dim intCount As Integer
+
+        intCount = l - strBuffer.Length()
+        If intCount > 0 Then
+            rightPad = String.Format("{0}{1}", strBuffer, Space(intCount))
+        Else
+            rightPad = strBuffer
+        End If
+        Return rightPad
+
+    End Function
 End Module
